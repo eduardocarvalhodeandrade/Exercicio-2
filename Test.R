892e32b11e3ab756584b3775e54f19c0f83e5c38
eduardocarvalhodeandrade <eduardocarvalhodeandrade2@gmail.com>
2026-03-30 17:22:39 -0300

    Adicionando o script principal inteiro

--- Test.R
+++ Test.R
@@ -1,1 +1,14 @@
-#real_shit
+library(ggplot2)
+dados = read.csv("carros.csv", header=T, sep=",")
+attach(dados)
+#Médias
+
+mean(consumo_km_l)
+mean(peso_kg)
+
+#Correlação
+
+ggplot(data = dados, aes(x = dados$consumo_km_l, y = dados$peso_kg)) + geom_point() + geom_smooth(method = "lm", se = TRUE)
+
+
+       
